3b7a328a16f79fafca133b65d4379f908287b8af
aliyul <waliprogrammer@gmail.com>
2025-03-21 23:26:07 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,26 +1,26 @@
- document.addEventListener("DOMContentLoaded", function() {
-    // Mendapatkan URL halaman saat ini
-    var currentUrl = window.location.href;
+  var currentUrl = window.location.href;
 
-    // Menentukan URL yang ingin Anda periksa
-    var targetUrl = "https://www.betonjayareadymix.com/2018/08/harga-jayamix-jayakerta.html";
+  // Cek URL yang diambil
+  console.log("Current URL:", currentUrl);
 
-    // Jika URL halaman saat ini cocok dengan target URL
-    if (currentUrl === targetUrl) {
-      // HTML yang ingin ditambahkan
-      var html = `
-        <a href='https://www.betonjayareadymix.com/p/material-konstruksi.html' itemprop='item' title='MATERIAL KONSTRUKSI'>
-            <span itemprop='name'>Material Konstruksi</span> &nbsp;&#8250;&nbsp;
-        </a>
+  // Menentukan URL yang ingin Anda periksa
+  var targetUrl = "https://www.betonjayareadymix.com/2018/08/harga-jayamix-jayakerta.html";
 
-        <a href='https://www.betonjayareadymix.com/p/ready-mix-beton-cor-jayamix-minimix.html' itemprop='item' title='READY MIX'>
-            <span itemprop='name'>Ready Mix</span> &nbsp;&#8250;&nbsp;
-        </a>
+  // Jika URL halaman saat ini cocok dengan target URL
+  if (currentUrl === targetUrl) {
+    // HTML yang ingin ditambahkan
+    var html = `
+      <a href='https://www.betonjayareadymix.com/p/material-konstruksi.html' itemprop='item' title='MATERIAL KONSTRUKSI'>
+          <span itemprop='name'>Material Konstruksi</span> &nbsp;&#8250;&nbsp;
+      </a>
 
-        <span itemprop='name'>${document.title}</span>
-      `;
+      <a href='https://www.betonjayareadymix.com/p/ready-mix-beton-cor-jayamix-minimix.html' itemprop='item' title='READY MIX'>
+          <span itemprop='name'>Ready Mix</span> &nbsp;&#8250;&nbsp;
+      </a>
 
-      // Menambahkan HTML ke body halaman
-      document.body.insertAdjacentHTML('afterbegin', html);
-    }
-  });
+      <span itemprop='name'>${document.title}</span>
+    `;
+
+    // Menambahkan HTML ke body halaman
+    document.body.insertAdjacentHTML('beforeend', html);
+  }
