8c8d9812ac189db2fef4215c7a624a72ccc952f3
aliyul <waliprogrammer@gmail.com>
2025-03-21 23:22:01 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,25 +1,26 @@
-// Mendapatkan URL halaman saat ini
-  var currentUrl = window.location.href;
+ document.addEventListener("DOMContentLoaded", function() {
+    // Mendapatkan URL halaman saat ini
+    var currentUrl = window.location.href;
 
-  // Menentukan URL yang ingin Anda periksa
-  var targetUrl = "https://www.betonjayareadymix.com/2018/08/harga-jayamix-jayakerta.html";
+    // Menentukan URL yang ingin Anda periksa
+    var targetUrl = "https://www.betonjayareadymix.com/2018/08/harga-jayamix-jayakerta.html";
 
-  // Jika URL halaman saat ini cocok dengan target URL
-  if (currentUrl === targetUrl) {
-    // HTML yang ingin ditambahkan
-    var html = `
-      <a href='https://www.betonjayareadymix.com/p/material-konstruksi.html' itemprop='item' title='MATERIAL KONSTRUKSI'>
-          <span itemprop='name'>Material Konstruksi</span> &nbsp;&#8250;&nbsp;
-      </a>
+    // Jika URL halaman saat ini cocok dengan target URL
+    if (currentUrl === targetUrl) {
+      // HTML yang ingin ditambahkan
+      var html = `
+        <a href='https://www.betonjayareadymix.com/p/material-konstruksi.html' itemprop='item' title='MATERIAL KONSTRUKSI'>
+            <span itemprop='name'>Material Konstruksi</span> &nbsp;&#8250;&nbsp;
+        </a>
 
-      <a href='https://www.betonjayareadymix.com/p/ready-mix-beton-cor-jayamix-minimix.html' itemprop='item' title='READY MIX'>
-          <span itemprop='name'>Ready Mix</span> &nbsp;&#8250;&nbsp;
-      </a>
+        <a href='https://www.betonjayareadymix.com/p/ready-mix-beton-cor-jayamix-minimix.html' itemprop='item' title='READY MIX'>
+            <span itemprop='name'>Ready Mix</span> &nbsp;&#8250;&nbsp;
+        </a>
 
-      <span itemprop='name'><data:blog.pageName/></span>
-     
-    `;
+        <span itemprop='name'>${document.title}</span>
+      `;
 
-    // Menambahkan HTML ke body halaman
-    document.body.insertAdjacentHTML('beforeend', html);
-  }
+      // Menambahkan HTML ke body halaman
+      document.body.insertAdjacentHTML('beforeend', html);
+    }
+  });
