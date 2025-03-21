6386dda57f91990bf47ecf468ea14f4746e00354
aliyul <waliprogrammer@gmail.com>
2025-03-21 23:32:40 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,13 +1,8 @@
+window.onload = function() {
   var currentUrl = window.location.href;
-
-  // Menentukan URL yang ingin Anda periksa
   var targetUrl = "https://www.betonjayareadymix.com/2018/08/harga-jayamix-jayakerta.html";
 
-  // Jika URL halaman saat ini cocok dengan target URL
   if (currentUrl === targetUrl) {
-    // HTML yang ingin ditambahkan
-      // Cek URL yang diambil
-  console.log("Current URL:", currentUrl);
     var html = `
       <a href='https://www.betonjayareadymix.com/p/material-konstruksi.html' itemprop='item' title='MATERIAL KONSTRUKSI'>
           <span itemprop='name'>Material Konstruksi</span> &nbsp;&#8250;&nbsp;
@@ -19,7 +14,6 @@
 
       <span itemprop='name'>${document.title}</span>
     `;
-
-    // Menambahkan HTML ke body halaman
     document.body.insertAdjacentHTML('beforeend', html);
   }
+};
