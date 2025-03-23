d489fbc326625a6656f766de8fdc9b62af1b371b
aliyul <waliprogrammer@gmail.com>
2025-03-23 21:36:09 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
     breadcrumbContainer.innerHTML = "<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='KATEGORI KONSTRUKSI'>KATEGORI KONSTRUKSI</a> &nbsp;&#8250;&nbsp; " + document.title;
   }
 });
+
+window.onload = function() {
+  if (window.location.href === "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html") {
+    var breadcrumbContainer = document.getElementById("breadcrumb");
+    breadcrumbContainer.innerHTML = "<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='KATEGORI KONSTRUKSI'>KATEGORI KONSTRUKSI</a> &nbsp;&#8250;&nbsp; " + document.title;
+  }
+};
