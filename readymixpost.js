59042c0ff40b2b273a50681e037fdd2c67167162
aliyul <waliprogrammer@gmail.com>
2025-03-23 20:49:01 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,28 +1,6 @@
-// Mendapatkan URL halaman saat ini
-var currentPageUrl = window.location.href;
-
-// Mendefinisikan URL target untuk breadcrumbs
-var targetUrl = "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html";
-
-// Mendefinisikan struktur breadcrumbs
-var breadcrumbs = [
-  { title: "BJR", url: "https://www.betonjayareadymix.com/" },
-  { title: "Jasa Konstruksi", url: "https://www.betonjayareadymix.com/p/jasa-konstruksi.html" },
-  { title: "Jasa Renovasi Perbaikan", url: "https://www.betonjayareadymix.com/p/jasa-renovasi-perbaikan.html" },
-   { title: "Jasa Chipping Beton", url: "https://www.betonjayareadymix.com/p/jasa-chipping-beton.html" }
-];
-
-// Memeriksa apakah URL halaman saat ini sama dengan URL target
-if (currentPageUrl === targetUrl) {
-  console.log();
-  // Menampilkan struktur breadcrumbs
-  document.write("<div class='breadcrumbs'>");
-  for (var i = 0; i < breadcrumbs.length; i++) {
-    if (i < breadcrumbs.length - 1) {
-      document.write("<a href='" + breadcrumbs[i].url + "'>" + breadcrumbs[i].title + "</a> &gt; ");
-    } else {
-      document.write(breadcrumbs[i].title);
-    }
-  }
-  document.write("</div>");
+if (window.location.href === "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html") {
+  document.write("<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='KATEGORI KONSTRUKSI'>");
+  document.write("<span itemprop='name'>KATEGORI KONSTRUKSI</span> &nbsp;&#8250;&nbsp; ");
+  document.write("</a>");
+  document.write("<span itemprop='name'>" + document.title + "</span>");
 }
