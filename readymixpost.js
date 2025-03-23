c03ba3776f2e261e2ca3a378857e27401b652c77
aliyul <waliprogrammer@gmail.com>
2025-03-23 21:23:21 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,23 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var targetUrl = "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html";
-  var breadcrumbTitle = "KATEGORI KONSTRUKSI";
-
-  if (window.location.href === targetUrl) {
-    document.write("<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='" + breadcrumbTitle + "'>");
-    document.write("<span itemprop='name'>" + breadcrumbTitle + "</span> &nbsp;&#8250;&nbsp; ");
-    document.write("</a>");
-    document.write("<span itemprop='name'>" + document.title + "</span>");
-  }
-});
-
-window.onload = function() {
-  var targetUrl = "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html";
-  var breadcrumbTitle = "KATEGORI KONSTRUKSI";
-
-  if (window.location.href === targetUrl) {
-    document.write("<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='" + breadcrumbTitle + "'>");
-    document.write("<span itemprop='name'>" + breadcrumbTitle + "</span> &nbsp;&#8250;&nbsp; ");
-    document.write("</a>");
-    document.write("<span itemprop='name'>" + document.title + "</span>");
-  }
-};
+<b:if cond='data:blog.url == &quot;https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html&quot;'>
+          <a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='KATEGORI KONSTRUKSI'>
+            <span itemprop='name'><data:/>KATEGORI KONSTRUKSI</span> &amp;nbsp;&#8250;&amp;nbsp;
+          </a>
+<span itemprop='name'><data:blog.pageName/></span>
+     
+</b:if>
