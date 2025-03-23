19c05127553d667122f67d457ef077666646938f
aliyul <waliprogrammer@gmail.com>
2025-03-23 21:29:34 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,9 +1,6 @@
-var targetUrl = "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html";
-var breadcrumbTitle = "KATEGORI KONSTRUKSI";
-
-if (window.location.href === targetUrl) {
-  document.write("<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='" + breadcrumbTitle + "'>");
-  document.write("<span itemprop='name'>" + breadcrumbTitle + "</span> &nbsp;&#8250;&nbsp; ");
-  document.write("</a>");
-  document.write("<span itemprop='name'>" + document.title + "</span>");
-}
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.location.href === "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html") {
+    var breadcrumbContainer = document.getElementById("breadcrumb");
+    breadcrumbContainer.innerHTML = "<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='KATEGORI KONSTRUKSI'>KATEGORI KONSTRUKSI</a> &nbsp;&#8250;&nbsp; " + document.title;
+  }
+});
