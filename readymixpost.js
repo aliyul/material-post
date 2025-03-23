5db39ce4b26985ffecf09573f27e24828fc51508
aliyul <waliprogrammer@gmail.com>
2025-03-23 21:59:08 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,13 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-  if (window.location.href === "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html") {
-    var breadcrumbContainer = document.getElementById("breadcrumb");
-    breadcrumbContainer.innerHTML = "<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='KATEGORI KONSTRUKSI'>KATEGORI KONSTRUKSI</a> &nbsp;&#8250;&nbsp; " + document.title;
-  }
-});
-
 window.onload = function() {
-  if (window.location.href === "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html") {
-    var breadcrumbContainer = document.getElementById("breadcrumb");
-    breadcrumbContainer.innerHTML = "<a href='https://www.betonjayareadymix.com/p/kategori-konstruksi.html' itemprop='item' title='KATEGORI KONSTRUKSI'>KATEGORI KONSTRUKSI</a> &nbsp;&#8250;&nbsp; " + document.title;
-  }
-};
+    var currentUrl = window.location.href;
+
+    // Tentukan elemen yang akan diubah
+    var materialKonstruksiLink = document.querySelector("a[title='MATERIAL KONSTRUKSI']");
+    var readyMixLink = document.querySelector("a[title='READY MIX']");
+    var pageNameSpan = document.querySelector("span[itemprop='name']");
+
+    // Default untuk menyembunyikan elemen
+    materialKonstruksiLink.style.visibility = 'hidden';
+    readyMixLink.style.visibility = 'hidden';
+    pageNameSpan.textContent = "Default Page";
+
+    // Cek URL saat ini dan sesuaikan dengan kondisi yang diinginkan
+    if (currentUrl === "https://www.betonjayareadymix.com/2021/08/jayamix-pangandaran.html") {
+      materialKonstruksiLink.style.visibility = 'visible';
+      readyMixLink.style.visibility = 'visible';
+      pageNameSpan.textContent = "Jayamix Pangandaran";
+    } else if (currentUrl === "https://www.betonjayareadymix.com/2021/08/jayamix-sumedang.html") {
+      materialKonstruksiLink.style.visibility = 'visible';
+      readyMixLink.style.visibility = 'visible';
+      pageNameSpan.textContent = "Jayamix Sumedang";
+    } else if (currentUrl === "https://www.betonjayareadymix.com/2021/08/jayamix-sukabumi.html") {
+      materialKonstruksiLink.style.visibility = 'visible';
+      readyMixLink.style.visibility = 'visible';
+      pageNameSpan.textContent = "Jayamix Sukabumi";
+    } else if (currentUrl === "https://www.betonjayareadymix.com/2021/08/jayamix-karawang.html") {
+      materialKonstruksiLink.style.visibility = 'visible';
+      readyMixLink.style.visibility = 'visible';
+      pageNameSpan.textContent = "Jayamix Karawang";
+    } else if (currentUrl === "https://www.betonjayareadymix.com/2021/08/jayamix-kuningan.html") {
+      materialKonstruksiLink.style.visibility = 'visible';
+      readyMixLink.style.visibility = 'visible';
+      pageNameSpan.textContent = "Jayamix Kuningan";
+    }
+  };
