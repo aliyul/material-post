112d974ff68611aceb72ed3ba16a788643b2d64b
aliyul <waliprogrammer@gmail.com>
2025-03-25 22:41:29 +0700

    Update readymixpost.js

--- readymixpost.js
+++ readymixpost.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     // var currentUrl = window.location.href;
      //const cleanUrl = currentUrl.split('?')[0]; // Menghapus parameter seperti ?m=1
-   // const cleanUrl = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
+    const cleanUrl = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
 
      // Menemukan elemen menggunakan ID
-     // var materialKonstruksiLink = document.getElementById("materialKonstruksi");
-     //var readyMixLink = document.getElementById("readyMix");
-     //var pageNameSpan = document.getElementById("pageNameSpan");
+      var materialKonstruksiLink = document.getElementById("materialKonstruksi");
+     var readyMixLink = document.getElementById("readyMix");
+     var pageNameSpan = document.getElementById("pageNameSpan");
 
      // Default untuk menyembunyikan elemen
-    // materialKonstruksiLink.style.visibility = 'hidden';
-     // readyMixLink.style.visibility = 'hidden';
-     // pageNameSpan.textContent = "";
+    materialKonstruksiLink.style.visibility = 'hidden';
+     readyMixLink.style.visibility = 'hidden';
+     pageNameSpan.textContent = "";
 
      // Cek URL saat ini dan sesuaikan dengan kondisi yang diinginkan
      const urlMapping = {
@@ -189,10 +189,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 };
-    var materialKonstruksiLink = document.getElementById("materialKonstruksi");
-    var readyMixLink = document.getElementById("readyMix");
-    var pageNameSpan = document.getElementById("pageNameSpan");
-
+  
 console.log('Material Konstruksi:', materialKonstruksiLink);
     console.log('Ready Mix:', readyMixLink);
     console.log('Page Name Span:', pageNameSpan);
