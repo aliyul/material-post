d2cff5d0a07c93614f1d503f2ea80f89fe2a2313
aliyul <waliprogrammer@gmail.com>
2025-03-26 10:31:39 +0700

    Update and rename readymixpost.js to readymix-post.js

--- readymix-post.js
+++ readymix-post.js
@@ -1,19 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // var currentUrl = window.location.href;
-     //const cleanUrl = currentUrl.split('?')[0]; // Menghapus parameter seperti ?m=1
-    const cleanUrl = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
-
-     // Menemukan elemen menggunakan ID
-      var materialKonstruksiLink = document.getElementById("materialKonstruksi");
-     var readyMixLink = document.getElementById("readyMix");
-     var pageNameSpan = document.getElementById("pageNameSpan");
-
-     // Default untuk menyembunyikan elemen
-     materialKonstruksiLink.style.visibility = 'hidden';
-     readyMixLink.style.visibility = 'hidden';
-     pageNameSpan.textContent = "";
-
-     // Cek URL saat ini dan sesuaikan dengan kondisi yang diinginkan
+ // Cek URL saat ini dan sesuaikan dengan kondisi yang diinginkan
      const urlMapping = {
     "https://www.betonjayareadymix.com/2021/08/jayamix-pangandaran.html": "Jayamix Pangandaran",
     "https://www.betonjayareadymix.com/2021/08/jayamix-sumedang.html": "Jayamix Sumedang",
@@ -189,8 +174,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 };
+document.addEventListener("DOMContentLoaded", function() {
+    // var currentUrl = window.location.href;
+     //const cleanUrl = currentUrl.split('?')[0]; // Menghapus parameter seperti ?m=1
+    const cleanUrl = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
+
+     // Menemukan elemen menggunakan ID
+      var materialKonstruksiLink = document.getElementById("materialKonstruksi");
+     var readyMixLink = document.getElementById("readyMix");
+     var pageNameSpan = document.getElementById("pageNameSpan");
+
+     // Default untuk menyembunyikan elemen
+     materialKonstruksiLink.style.visibility = 'hidden';
+     readyMixLink.style.visibility = 'hidden';
+     pageNameSpan.textContent = "";
   
-console.log('Material Konstruksi:', materialKonstruksiLink);
+    console.log('Material Konstruksi:', materialKonstruksiLink);
     console.log('Ready Mix:', readyMixLink);
     console.log('Page Name Span:', pageNameSpan);
 
