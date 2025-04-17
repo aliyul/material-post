0e358732521404a6d014b6332cdf593f497a1f2c
aliyul <waliprogrammer@gmail.com>
2025-04-17 17:45:00 +0700

    Update material-struktur-bangunan-post.js

--- material-struktur-bangunan-post.js
+++ material-struktur-bangunan-post.js
@@ -782,14 +782,14 @@
 
 };
 // Menyimpan elemen yang dihapus dalam variabel
-let removedElements = {};
+let removedElementsMaterialKonsStukturPost = {};
 // Fungsi untuk menghapus elemen berdasarkan ID
 function removeCondition(conditionId) {
     const conditionElement = document.getElementById(conditionId);
 
     if (conditionElement) {
         // Menyimpan elemen yang dihapus dalam objek untuk bisa dikembalikan
-        removedElements[conditionId] = conditionElement;
+        removedElementsMaterialKonsStukturPost[conditionId] = conditionElement;
         conditionElement.remove(); // Menghapus elemen tersebut
     }
 }
@@ -797,13 +797,13 @@ function removeCondition(conditionId) {
 // Fungsi untuk mengembalikan elemen yang telah dihapus
 function restoreCondition(conditionId) {
     const breadcrumb = document.querySelector('.breadcrumb');
-    const elementToRestore = removedElements[conditionId]; // Mendapatkan elemen yang disimpan
+    const elementToRestore = removedElementsMaterialKonsStukturPost[conditionId]; // Mendapatkan elemen yang disimpan
 
     if (elementToRestore) {
         breadcrumb.appendChild(elementToRestore); // Menambahkan elemen kembali ke dalam breadcrumb
-        delete removedElements[conditionId]; // Menghapus elemen dari objek setelah dikembalikan
+        delete removedElementsMaterialKonsStukturPost[conditionId]; // Menghapus elemen dari objek setelah dikembalikan
     } else {
-        console.log(`Elemen dengan ID ${conditionId} tidak ditemukan di removedElements.`);
+        console.log(`Elemen dengan ID ${conditionId} tidak ditemukan di removedElementsMaterialKonsStukturPost.`);
     }
 }
 
