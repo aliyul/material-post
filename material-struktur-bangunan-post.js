d61b62b939583f162a33423efdd0aec4a1729f85
aliyul <waliprogrammer@gmail.com>
2025-04-26 16:15:55 +0700

    Update material-struktur-bangunan-post.js

--- material-struktur-bangunan-post.js
+++ material-struktur-bangunan-post.js
@@ -1697,4 +1697,48 @@ if (urlMappingSemenPutihPost[cleanUrl]) {
         pageNameMaterialKonsStukturPost.textContent = urlMappingSemenPutihPost[cleanUrl];
     }
 
+if (urlMappingBesiBangunanPost[cleanUrl]) {
+       restoreCondition('MaterialKonsStukturPost');
+       restoreCondition('BesiBangunan');
+      
+    // hapus ID DIV Lain
+        removeCondition('ProdukKonsSaluranPost');
+        removeCondition('ProdukKonsPembatasPost');
+        removeCondition('JasaKonsPembatasPost');
+        removeCondition('JasaKonsPondasiTanahPost');
+        removeCondition('JasaKonsPerkuatanTanahLongsorPost');
+        removeCondition('JasaKonsPerbaikanPost');
+	removeCondition('JasaKonsBongkarBangunanPost');
+        removeCondition('JasaKonsPengeboranPost'); 
+	removeCondition('JasaKonsFinishingPost');
+        removeCondition('JasaKonsStrukturPost');
+        removeCondition('MaterialKonsFasadPelapisEksteriorPost');
+   
+    //hapus elemen lain nya selain READY MIX
+     removeCondition('SemenPortland');
+     removeCondition('BesiBetonUlir');
+     removeCondition('BesiBetonPolos');
+     removeCondition('Wiremesh');
+     removeCondition('Bondex');
+     removeCondition('SemenPutih');
+     removeCondition('BajaKonvensional');
+     removeCondition('BajaRinganStruktur');
+     removeCondition('BajaTulangan');
+     removeCondition('Scaffolding');
+     removeCondition('BekistingBaja');
+     removeCondition('SemenInstan');
+     removeCondition('readyMix');
+     removeCondition('BekistingKayu');
+     removeCondition('MortarStruktural');
+     removeCondition('PerekatBetonEpoxy');
+    
+       // restoreCondition('MaterialKonsStukturPost');
+       
+       MaterialKonsStukturPost.style.visibility = 'visible';
+       MaterialKonstruksiStukturPostLink.style.visibility = 'visible';
+       
+        MaterialStrukturBangunanPostLink.style.visibility = 'visible';
+        BesiBangunanLink.style.visibility = 'visible';
+        pageNameMaterialKonsStukturPost.textContent = urlMappingBesiBangunanPost[cleanUrl];
+    }
    });
