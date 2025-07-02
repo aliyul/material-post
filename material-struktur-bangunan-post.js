0291fb55ea63566fd1f554125763f8806df03de0
aliyul <waliprogrammer@gmail.com>
2025-07-02 15:18:44 +0700

    Update material-struktur-bangunan-post.js

--- material-struktur-bangunan-post.js
+++ material-struktur-bangunan-post.js
@@ -1154,8 +1154,14 @@ const urlMappingBajaTulanganPost = {
     "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-per-kg.html": "Harga Besi Beton per Kg",
   "https://www.betonjayareadymix.com/2019/05/daftar-harga-besi-beton.html": "Daftar Harga Besi Beton",
   "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-12mm-per-batang.html": "Harga Besi Beton 12mm per Batang",
-  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-surabaya.html": "Harga Besi Beton Surabaya",
-  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-jogja.html": "Harga Besi Beton Jogja",
+    "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-garut.html": "Harga Besi Beton Garut",
+    "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-bandung.html": "Harga Besi Beton Bandung",
+  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-karawang.html": "Harga Besi Beton Karawang",
+  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-bekasi.html": "Harga Besi Beton Bekasi",
+  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-tangerang.html": "Harga Besi Beton Tangerang",
+  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-depok.html": "Harga Besi Beton Depok",
+  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-bogor.html": "Harga Besi Beton Bogor",
+  "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-jakarta.html": "Harga Besi Beton Jakarta",
   "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-10mm.html": "Harga Besi Beton 10mm",
   "https://www.betonjayareadymix.com/2019/05/harga-besi-beton-12mm.html": "Harga Besi Beton 12mm"
 };
